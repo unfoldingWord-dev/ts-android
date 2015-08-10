ad988eac0978805f210e5cc8cd02d463fb209741
Joel Lonbeck <joel@neutrinographics.com>
2015-08-10 01:20:42 -0700

    updated source

--- app/src/main/java/com/door43/translationstudio/projects/data/DataStore.java
+++ app/src/main/java/com/door43/translationstudio/projects/data/DataStore.java
@@ -39,7 +39,7 @@ public class DataStore {
     private static SharedPreferences mSettings;
     private static final int API_VERSION = 2;
     // this is used so we can force a cache reset between versions of the app if we make changes to api implimentation
-    private static final int API_VERSION_INTERNAL = 10;
+    private static final int API_VERSION_INTERNAL = 11;
 
     public DataStore(MainApplication context) {
         mContext = context;
