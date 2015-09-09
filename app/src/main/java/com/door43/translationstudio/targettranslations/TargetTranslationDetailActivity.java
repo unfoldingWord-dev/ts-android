2e3c35ed4af2cf921ad271c04d33c4fdddd136da
Joel Lonbeck <joel@neutrinographics.com>
2015-09-08 17:46:41 -0700

    added side toolbars

--- app/src/main/java/com/door43/translationstudio/targettranslations/TargetTranslationDetailActivity.java
+++ app/src/main/java/com/door43/translationstudio/targettranslations/TargetTranslationDetailActivity.java
@@ -15,6 +15,7 @@ public class TargetTranslationDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target_translation_detail);
+
     }
 
     @Override
