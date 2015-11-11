f00ec6728a253d4c1f4eba4f22b05b2c916f27f1
Joel Lonbeck <joel@neutrinographics.com>
2015-11-11 11:00:42 -0800

    added basic support for restoring from server backups

--- app/src/main/java/com/door43/translationstudio/newui/home/RestoreFromCloudDialog.java
+++ app/src/main/java/com/door43/translationstudio/newui/home/RestoreFromCloudDialog.java
@@ -14,13 +14,21 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.door43.tools.reporting.Logger;
 import com.door43.translationstudio.AppContext;
 import com.door43.translationstudio.R;
+import com.door43.translationstudio.core.Library;
+import com.door43.translationstudio.core.TargetLanguage;
+import com.door43.translationstudio.core.TargetTranslation;
 import com.door43.translationstudio.core.Translator;
+import com.door43.translationstudio.tasks.CloneTargetTranslationTask;
 import com.door43.translationstudio.tasks.GetCloudBackupsTask;
 import com.door43.util.tasks.GenericTaskWatcher;
 import com.door43.util.tasks.ManagedTask;
 import com.door43.util.tasks.TaskManager;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by joel on 11/6/2015.
@@ -31,6 +39,7 @@ public class RestoreFromCloudDialog extends DialogFragment implements GenericTas
     private RestoreFromCloudAdapter adapter;
     private String[] targetTranslationSlugs = new String[0];
     private Translator translator;
+    private Library library;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -40,6 +49,7 @@ public class RestoreFromCloudDialog extends DialogFragment implements GenericTas
         this.taskWatcher.setOnFinishedListener(this);
 
         this.translator = AppContext.getTranslator();
+        this.library = AppContext.getLibrary();
 
         Button dismissButton = (Button)v.findViewById(R.id.dismiss_button);
         dismissButton.setOnClickListener(new View.OnClickListener() {
@@ -65,26 +75,46 @@ public class RestoreFromCloudDialog extends DialogFragment implements GenericTas
                 String targetTranslationSlug = adapter.getItem(position);
                 // check if the user already has this target translation
                 if(translator.getTargetTranslation(targetTranslationSlug) != null) {
-                    // TODO: confirm restore then just clone to a temporary location and just replace the current state. or do we want the entire history to be overwritten?
+                    // clone target translation
+                    File destDir;
+                    try {
+                        destDir = File.createTempFile(targetTranslationSlug, "");
+                    } catch (IOException e) {
+                        Logger.e(RestoreFromCloudDialog.class.getName(), "Could not create a temp directory for cloning", e);
+                        return;
+                    }
+                    CloneTargetTranslationTask task = new CloneTargetTranslationTask(targetTranslationSlug, destDir);
+                    taskWatcher.watch(task);
+                    TaskManager.addTask(task, CloneTargetTranslationTask.TASK_ID);
+                    // TODO: 11/11/2015 set flag so we know we need to copy the HEAD of the temp clone onto the existing repo
                 } else {
-                    // TODO: clone backup
+                    // clone target translation
+                    File destDir = TargetTranslation.generateTargetTranslationDir(targetTranslationSlug, translator.getPath());
+                    CloneTargetTranslationTask task = new CloneTargetTranslationTask(targetTranslationSlug, destDir);
+                    taskWatcher.watch(task);
+                    TaskManager.addTask(task, CloneTargetTranslationTask.TASK_ID);
                 }
             }
         });
 
         // connect to existing task
-        GetCloudBackupsTask task = (GetCloudBackupsTask)TaskManager.getTask(GetCloudBackupsTask.TASK_ID);
-        if(task != null) {
-            taskWatcher.watch(task);
+        GetCloudBackupsTask checkTask = (GetCloudBackupsTask)TaskManager.getTask(GetCloudBackupsTask.TASK_ID);
+        CloneTargetTranslationTask cloneTask = (CloneTargetTranslationTask)TaskManager.getTask(CloneTargetTranslationTask.TASK_ID);
+        if(checkTask != null) {
+            taskWatcher.watch(checkTask);
             // display loading icon
+        } else if(cloneTask != null) {
+            taskWatcher.watch(cloneTask);
         }
 
         if(savedInstanceState == null) {
-            // start task
-            task = new GetCloudBackupsTask();
-            taskWatcher.watch(task);
-            TaskManager.addTask(task, GetCloudBackupsTask.TASK_ID);
-        } else if (task == null) {
+            if(cloneTask == null && checkTask == null) {
+                // start task
+                checkTask = new GetCloudBackupsTask();
+                taskWatcher.watch(checkTask);
+                TaskManager.addTask(checkTask, GetCloudBackupsTask.TASK_ID);
+            }
+        } else if (checkTask == null) {
             // load existing data
             targetTranslationSlugs = savedInstanceState.getStringArray(STATE_TARGET_TRANSLATIONS);
             adapter.setTargetTranslations(targetTranslationSlugs);
@@ -97,23 +127,29 @@ public class RestoreFromCloudDialog extends DialogFragment implements GenericTas
     public void onFinished(ManagedTask task) {
         taskWatcher.stop();
         TaskManager.clearTask(task);
-        targetTranslationSlugs = ((GetCloudBackupsTask)task).getTargetTranslationSlugs();
-        Handler hand = new Handler(Looper.getMainLooper());
-        hand.post(new Runnable() {
-            @Override
-            public void run() {
-                if(targetTranslationSlugs.length > 0) {
-                    adapter.setTargetTranslations(targetTranslationSlugs);
-                } else {
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle(R.string.import_from_online)
-                            .setMessage(R.string.no_backups_online)
-                            .setNeutralButton(R.string.dismiss, null)
-                            .show();
-                    dismiss();
+
+        if(task instanceof GetCloudBackupsTask) {
+            targetTranslationSlugs = ((GetCloudBackupsTask) task).getTargetTranslationSlugs();
+            Handler hand = new Handler(Looper.getMainLooper());
+            hand.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (targetTranslationSlugs.length > 0) {
+                        adapter.setTargetTranslations(targetTranslationSlugs);
+                    } else {
+                        new AlertDialog.Builder(getActivity())
+                                .setTitle(R.string.import_from_online)
+                                .setMessage(R.string.no_backups_online)
+                                .setNeutralButton(R.string.dismiss, null)
+                                .show();
+                        dismiss();
+                    }
                 }
-            }
-        });
+            });
+        } else if(task instanceof CloneTargetTranslationTask) {
+            // TODO: 11/11/2015 copy HEAD if this repo already exists.
+            // TODO: 11/11/2015 reload the parent activity
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/door43/translationstudio/tasks/CloneTargetTranslationTask.java
@@ -0,0 +1,59 @@
+package com.door43.translationstudio.tasks;
+
+import com.door43.tools.reporting.Logger;
+import com.door43.translationstudio.AppContext;
+import com.door43.translationstudio.R;
+import com.door43.translationstudio.SettingsActivity;
+import com.door43.translationstudio.git.TransportCallback;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+import java.io.File;
+
+/**
+ * Clones a repository from the server
+ */
+public class CloneTargetTranslationTask extends SshTask {
+
+    public static final String TASK_ID = "clone_target_translation";
+    private final File localPath;
+    private final String remote;
+
+    public CloneTargetTranslationTask(String targetTranslationSlug, File dest) {
+        this.localPath = dest;
+        String server = AppContext.context().getUserPreferences().getString(SettingsActivity.KEY_PREF_GIT_SERVER, AppContext.context().getResources().getString(R.string.pref_default_git_server));
+        this.remote = server + ":tS/" + AppContext.udid() + "/" + targetTranslationSlug;
+    }
+
+    /**
+     * Returns the path where the repository was cloned
+     * @return
+     */
+    public File getLocalPath() {
+        return localPath;
+    }
+
+    @Override
+    protected void onRegistered() {
+        publishProgress(-1, AppContext.context().getResources().getString(R.string.downloading));
+        try {
+            // prepare destination
+            localPath.delete();
+
+            Git result = Git.cloneRepository()
+                    .setURI(remote)
+                    .setTransportConfigCallback(new TransportCallback())
+                    .setDirectory(localPath)
+                    .call();
+            result.getRepository().close();
+        } catch (Exception e) {
+            Logger.e(this.getClass().getName(), "Failed to clone the repository " + remote, e);
+            FileUtils.deleteQuietly(localPath);
+            stop();
+        }
+    }
+}
