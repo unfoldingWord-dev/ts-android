5ddfcb3d2f123900908abc569943c0957b5c5cbc
Joel Lonbeck <joel@neutrinographics.com>
2015-11-11 12:31:22 -0800

    added note

--- app/src/main/java/com/door43/translationstudio/newui/BackupDialog.java
+++ app/src/main/java/com/door43/translationstudio/newui/BackupDialog.java
@@ -66,6 +66,9 @@ public class BackupDialog extends DialogFragment implements GenericTaskWatcher.O
             throw new InvalidParameterException("The target translation id was not specified");
         }
 
+        // TODO: 11/11/2015 check if at least one translator has been recorded on this target translation
+        // if there are no translators the user must be presented with a form to enter a translator.
+
         Button backupToCloudButton = (Button)v.findViewById(R.id.upload_to_cloud);
         Button backupToSDButton = (Button)v.findViewById(R.id.export_to_sd);
         Button backupToAppButton = (Button)v.findViewById(R.id.export_to_app);
