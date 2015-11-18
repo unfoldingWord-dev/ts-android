cbe0f3fb6da8d158302895c1c30da824fda51c36
Bruce McLean <mcleanb@photo-nomad.net>
2015-11-18 14:49:36 -0500

    Add always share setting to general menu.

--- app/src/main/java/com/door43/translationstudio/SettingsActivity.java
+++ app/src/main/java/com/door43/translationstudio/SettingsActivity.java
@@ -57,6 +57,7 @@ public class SettingsActivity extends PreferenceActivity {
     public static final String KEY_PREF_GIT_SERVER = "git_server";
     public static final String KEY_PREF_GIT_SERVER_PORT = "git_server_port";
     public static final String KEY_PREF_REMEMBER_POSITION = "remember_position";
+    public static final String KEY_PREF_ALWAYS_SHARE = "always_share";
     public static final String KEY_PREF_MEDIA_SERVER = "media_server";
 //    public static final String KEY_PREF_EXPORT_FORMAT = "export_format";
     public static final String KEY_PREF_TRANSLATION_TYPEFACE = "translation_typeface";
