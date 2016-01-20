d6213d9f3a2fb689f3614aa931b77bc47e67170f
Joel Lonbeck <joel@neutrinographics.com>
2016-01-20 13:31:35 -0800

    added loading dialog to draft import

--- app/src/main/java/com/door43/translationstudio/newui/draft/DraftActivity.java
+++ app/src/main/java/com/door43/translationstudio/newui/draft/DraftActivity.java
@@ -1,35 +1,33 @@
 package com.door43.translationstudio.newui.draft;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
-import com.door43.tools.reporting.Logger;
 import com.door43.translationstudio.AppContext;
 import com.door43.translationstudio.R;
 import com.door43.translationstudio.core.Library;
 import com.door43.translationstudio.core.SourceTranslation;
 import com.door43.translationstudio.core.TargetTranslation;
-import com.door43.translationstudio.core.TranslationViewMode;
 import com.door43.translationstudio.core.Translator;
 import com.door43.translationstudio.dialogs.CustomAlertDialog;
 import com.door43.translationstudio.newui.BaseActivity;
-import com.door43.translationstudio.newui.translate.ViewModeFragment;
+import com.door43.translationstudio.tasks.ImportDraftTask;
+import com.door43.util.tasks.GenericTaskWatcher;
+import com.door43.util.tasks.ManagedTask;
+import com.door43.util.tasks.TaskManager;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class DraftActivity extends BaseActivity{
+public class DraftActivity extends BaseActivity implements GenericTaskWatcher.OnFinishedListener {
     public static final String TAG = "DraftActivity";
     public static final String EXTRA_TARGET_TRANSLATION_ID = "target_translation_id";
     private TargetTranslation mTargetTranslation;
@@ -39,6 +37,7 @@ public class DraftActivity extends BaseActivity{
     private RecyclerView mRecylerView;
     private LinearLayoutManager mLayoutManager;
     private DraftAdapter mAdapter;
+    private GenericTaskWatcher taskWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +75,9 @@ public class DraftActivity extends BaseActivity{
         mAdapter = new DraftAdapter(this, mDraftTranslations.get(0));
         mRecylerView.setAdapter(mAdapter);
 
+        taskWatcher = new GenericTaskWatcher(this, R.string.loading);
+        taskWatcher.setOnFinishedListener(this);
+
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,14 +90,20 @@ public class DraftActivity extends BaseActivity{
                             @Override
                             public void onClick(View v) {
                                 // // TODO: 1/20/2016 use the correct draft translation
-                                // TODO: 1/20/2016 run this in a task
-                                mTranslator.importDraftTranslation(mDraftTranslations.get(0), mLibrary);
-                                finish();
+                                ImportDraftTask task = new ImportDraftTask(mDraftTranslations.get(0));
+                                taskWatcher.watch(task);
+                                TaskManager.addTask(task, ImportDraftTask.TASK_ID);
                             }
                         })
                         .show("confirm_draft_import");
             }
         });
+
+        // re-attach to tasks
+        ManagedTask task = TaskManager.getTask(ImportDraftTask.TASK_ID);
+        if(task != null) {
+            taskWatcher.watch(task);
+        }
     }
 
     @Override
@@ -106,5 +114,27 @@ public class DraftActivity extends BaseActivity{
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFinished(ManagedTask task) {
+        taskWatcher.stop();
+        TaskManager.clearTask(task);
+        TargetTranslation targetTranslation = ((ImportDraftTask) task).getTargetTranslation();
+        if(targetTranslation != null) {
+            finish();
+        } else {
+            CustomAlertDialog.Create(this)
+                    .setTitle(R.string.error)
+                    .setMessage(R.string.translation_import_failed)
+                    .setNeutralButton(R.string.dismiss, null)
+                    .show("draft-import-failed");
+        }
+    }
+    @Override
+    public void onDestroy() {
+        taskWatcher.stop();
+
+        super.onDestroy();
     }
 }
