d6bbb50574a9cdd757c1fd68ffa32abd04563bed
Bruce McLean <mcleanb@photo-nomad.net>
2016-12-12 15:25:30 -0500

    TargetTranslationListFragment added sorting contols.

--- app/src/main/java/com/door43/translationstudio/ui/home/TargetTranslationListFragment.java
+++ app/src/main/java/com/door43/translationstudio/ui/home/TargetTranslationListFragment.java
@@ -8,18 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.door43.translationstudio.App;
 import com.door43.translationstudio.R;
 import com.door43.translationstudio.core.TargetTranslation;
 import com.door43.translationstudio.ui.BaseFragment;
 
+import org.unfoldingword.tools.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Displays a list of target translations
  */
 public class TargetTranslationListFragment extends BaseFragment implements TargetTranslationInfoDialog.OnDeleteListener {
 
+    public static final String TAG = TargetTranslationListFragment.class.getSimpleName();
     private TargetTranslationAdapter mAdapter;
     private OnItemClickListener mListener;
 
@@ -61,6 +69,51 @@ public class TargetTranslationListFragment extends BaseFragment implements Targe
                 mListener.onItemClick(mAdapter.getItem(position));
             }
         });
+
+        Spinner sortColumnSpinner = (Spinner) rootView.findViewById(R.id.sort_column);
+        if(sortColumnSpinner != null) {
+            List<String> types = new ArrayList<String>();
+            types.add(this.getResources().getString(R.string.sort_project_then_language));
+            types.add(this.getResources().getString(R.string.sort_language_then_project));
+            types.add(this.getResources().getString(R.string.sort_progress_then_project));
+            ArrayAdapter<String> typesAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, types);
+            typesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            sortColumnSpinner.setAdapter(typesAdapter);
+
+            sortColumnSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Logger.i(TAG, "Sort column item selected: " + position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        Spinner sortProjectSpinner = (Spinner) rootView.findViewById(R.id.sort_projects);
+        if(sortProjectSpinner != null) {
+            List<String> types = new ArrayList<String>();
+            types.add(this.getResources().getString(R.string.sort_bible_order));
+            types.add(this.getResources().getString(R.string.sort_alphabetical_order));
+            ArrayAdapter<String> typesAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, types);
+            typesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            sortProjectSpinner.setAdapter(typesAdapter);
+
+            sortProjectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Logger.i(TAG, "Sort project column item selected: " + position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
 
         // attach to info dialogs
         if(savedInstanceState != null) {
