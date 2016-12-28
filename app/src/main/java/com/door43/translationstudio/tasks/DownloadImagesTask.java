db3401b9e31a1dffa0e987de08b82331117edf55
Bruce McLean <mcleanb@photo-nomad.net>
2016-12-28 15:22:27 -0500

    Restored old Download Images code from older version.

--- app/src/main/java/com/door43/translationstudio/tasks/DownloadImagesTask.java
+++ app/src/main/java/com/door43/translationstudio/tasks/DownloadImagesTask.java
@@ -2,8 +2,9 @@ package com.door43.translationstudio.tasks;
 
 import com.door43.translationstudio.App;
 import com.door43.translationstudio.R;
+import com.door43.translationstudio.core.DownloadImages;
 
-import org.unfoldingword.door43client.Door43Client;
+import org.unfoldingword.tools.logger.Logger;
 import org.unfoldingword.tools.taskmanager.ManagedTask;
 
 /**
@@ -13,36 +14,41 @@ public class DownloadImagesTask extends ManagedTask {
 
     public static final String TASK_ID = "download_images_task";
 
-    private final Door43Client mLibrary;
     private int mMaxProgress = 100;
     private boolean mSuccess;
 
     public DownloadImagesTask() {
-        mLibrary = App.getLibrary();
+        mSuccess = false;
     }
 
     @Override
     public void start() {
-//        mSuccess = mLibrary.downloadImages(new Library.OnProgressListener() {
-//
-//            @Override
-//            public boolean onProgress(int progress, int max) {
-//                mMaxProgress = max;
-//                String message = String.format("%2.2f %s %2.2f %s",
-//                        progress / (1024f * 1024f),
-//                        App.context().getResources().getString(R.string.out_of),
-//                        max / (1024f * 1024f),
-//                        App.context().getResources().getString(R.string.mb_downloaded));
-//                publishProgress((float)progress / (float)max, message);
-//                return !isCanceled();
-//            }
-//
-//            @Override
-//            public boolean onIndeterminate() {
-//                publishProgress(-1, "");
-//                return !isCanceled();
-//            }
-//        });
+        mSuccess = false;
+        try {
+            DownloadImages downloadImages = new DownloadImages();
+            mSuccess = downloadImages.download(new DownloadImages.OnProgressListener() {
+
+                @Override
+                public boolean onProgress(int progress, int max) {
+                    mMaxProgress = max;
+                    String message = String.format("%2.2f %s %2.2f %s",
+                            progress / (1024f * 1024f),
+                            App.context().getResources().getString(R.string.out_of),
+                            max / (1024f * 1024f),
+                            App.context().getResources().getString(R.string.mb_downloaded));
+                    publishProgress((float) progress / (float) max, message);
+                    return !isCanceled();
+                }
+
+                @Override
+                public boolean onIndeterminate() {
+                    publishProgress(-1, "");
+                    return !isCanceled();
+                }
+            });
+        } catch (Exception e) {
+            Logger.e(this.getClass().getSimpleName(),"Download Failed", e);
+        }
 
         publishProgress(-1, "");
     }
