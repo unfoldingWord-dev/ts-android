e4296838937e1c6280b957050ba43738aae1eff2
Joel Lonbeck <joel@neutrinographics.com>
2019-04-29 23:59:49 -0700

    added usfm chunking

--- app/src/main/java/com/door43/util/Usfm.java
+++ app/src/main/java/com/door43/util/Usfm.java
@@ -1,9 +1,18 @@
 package com.door43.util;
 
+import android.util.JsonReader;
+
 import com.door43.translationstudio.ui.spannables.USFMVerseSpan;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.unfoldingword.door43client.models.ChunkMarker;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +26,45 @@ public class Usfm {
     private static final Pattern USFM_TAG_PATTERN = Pattern.compile("\\\\([\\w\\d]+)\\s([^\\n\\\\]*)");
 
     private Usfm() {
+    }
+
+    /**
+     * Splits a usfm into chunks based on the chunk markers.
+     *
+     * @param usfm
+     * @param markers
+     */
+    public static JSONArray chunkBook(String usfm, List<ChunkMarker> markers) throws JSONException {
+        Map<Integer, Map> parsedUsfm = parseBook(usfm);
+        JSONArray chunks = new JSONArray();
+        int index = 0;
+        for (ChunkMarker marker : markers) {
+            Boolean lastChunkOfChapter = index + 1 >= markers.size() || !markers.get(index + 1).chapter.equals(marker.chapter);
+            int chapter = Integer.valueOf(marker.chapter);
+            int firstVerse = Integer.valueOf(marker.verse);
+            int lastVerse = lastChunkOfChapter ? Integer.MAX_VALUE : Integer.parseInt(markers.get(index + 1).verse) - 1;
+
+            if(parsedUsfm.containsKey(chapter)) {
+                StringBuilder chunkContent = new StringBuilder();
+                for(Integer i = firstVerse; i <= lastVerse; i ++) {
+                    String verseText = (String)parsedUsfm.get(chapter).get(i);
+                    if(verseText == null) break;
+                    chunkContent.append("\\v").append(i).append(" ");
+                    chunkContent.append(verseText);
+                    chunkContent.append("\n");
+                }
+                JSONObject chunk = new JSONObject();
+                chunk.put("chapter", marker.chapter);
+                chunk.put("verse", marker.verse);
+                chunk.put("content", chunkContent.toString().trim());
+                chunks.put(chunk);
+            }
+
+            index++;
+        }
+
+        // TODO: add book and chapter titles
+        return chunks;
     }
 
     /**
@@ -95,7 +143,7 @@ public class Usfm {
                 CharSequence introUsfm = usfm.subSequence(0, matcher.start());
                 // TRICKY: don't include the intro if it's just some tags
                 String cleanedIntro = stripMarkup(introUsfm.toString(), new String[]{"p"});
-                if(!cleanedIntro.isEmpty()) {
+                if (!cleanedIntro.isEmpty()) {
                     verses.put(0, introUsfm.toString().trim());
                 }
             }
--- app/src/test/java/com/door43/util/UsfmTest.java
+++ app/src/test/java/com/door43/util/UsfmTest.java
@@ -1,7 +1,12 @@
 package com.door43.util;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.Test;
+import org.unfoldingword.door43client.models.ChunkMarker;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -34,5 +39,34 @@ public class UsfmTest {
         assertEquals("In the beginning, God created the heavens and the earth.", results.get(1).get(1));
         assertEquals("Then the heavens and the earth were finished, and all the living things that filled them.", results.get(2).get(1));
         assertEquals("Genesis", results.get(0).get("title"));
+    }
+
+    @Test
+    public void chunkUsfm() throws Exception {
+        String usfm = "\\id GEN unfoldingWord Literal Text\n" +
+                "\\ide UTF-8\n" +
+                "\\h Genesis\n" +
+                "\\toc1 The Book of Genesis\n" +
+                "\\toc2 Genesis\n" +
+                "\\toc3 Gen\n" +
+                "\\mt Genesis \n" +
+                "\n" +
+                "\\s5\n" +
+                "\\c 1\n" +
+                "\\p\n" +
+                "\\v 1 In the beginning, God created the heavens and the earth. \n" +
+                "\\v 2 The earth was without form and empty. Darkness was upon the surface of the deep. The Spirit of God was moving above the surface of the waters.\n" +
+                "\\s5\n" +
+                "\\c 2\n" +
+                "\\p\n" +
+                "\\v 1 Then the heavens and the earth were finished, and all the living things that filled them.\n" +
+                "\\v 2 On the seventh day God came to the end of his work which he had done, and so he rested on the seventh day from all his work.\n" +
+                "\\v 3 God blessed the seventh day and sanctified it, because in it he rested from all his work which he had done in his creation.\n";
+        List<ChunkMarker> chunks = new ArrayList<>();
+        chunks.add(new ChunkMarker("01", "01"));
+        chunks.add(new ChunkMarker("02", "01"));
+        chunks.add(new ChunkMarker("02", "02"));
+        JSONArray results = Usfm.chunkBook(usfm, chunks);
+        assertEquals(3, results.length());
     }
 }
